188796af6d507b77e936849c599355f3fd355b06
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-04-01 17:54:00 +0800

    Update user_resource.dart

--- lib/resource/user_resource.dart
+++ lib/resource/user_resource.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:my_blog_app/models/default_response_model.dart';
 import 'package:my_blog_app/models/login_request_model.dart';
 import 'package:my_blog_app/models/register_request_model.dart';
+import 'package:my_blog_app/models/user_response_model.dart';
 import 'package:my_blog_app/services/resource.dart';
 
 class UserResource {
@@ -24,7 +25,7 @@ class UserResource {
       url: 'login',
       data: loginRequestModel.toJson(),
       parse: (response) {
-        return DefaultResponseModel(
+        return UserResponseModel(
           json.decode(response.body),
         );
       },
